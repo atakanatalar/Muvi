2710631b659b22cbfdb71898f786ea68c10e6fad
Atakan Atalar <atakanatalar@icloud.com>
2024-08-19 11:44:50 +0300

    Initialize project

---
+++ Muvi/Muvi/MuviApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuviApp.swift
+//  Muvi
+//
+//  Created by Atakan Atalar on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MuviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
